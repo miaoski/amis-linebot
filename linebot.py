857d1afdc169161f1aa8291fd63e10fa20e36354
miaoski <lin.zhemin@gmail.com>
2016-04-19 15:35:38 +0800

    add FB bot

--- linebot.py
+++ linebot.py
@@ -8,6 +8,8 @@ import logging
 import ConfigParser
 from fuzzy import fuzzme
 import requests
+import json
+import amis
 
 app = flask.Flask(__name__)
 app.logger.setLevel(logging.DEBUG)
@@ -15,6 +17,7 @@ app.logger.setLevel(logging.DEBUG)
 DATABASE = 'dict-amis.sq3'
 LINE_ENDPOINT = "https://trialbot-api.line.me"
 USER_LASTWORD = {}
+RE_NUM = re.compile(r'[0-9]+')
 
 def connect_db():
     return sqlite3.connect(DATABASE)
@@ -32,10 +35,43 @@ def teardown_request(exception):
 def homepage():
     return 'Welcome to Amis LINE BOT', 200
 
+@app.route('/fb', methods=['GET', 'POST'])
+def fbbot():
+    if flask.request.method == 'GET':
+        if flask.request.args.get('hub.verify_token') == FB_TOKEN:
+            return flask.request.args.get('hub.challenge'), 200
+    if flask.request.method == 'POST':
+        db = amis.loaddb()
+        try:
+            app.logger.info(flask.request.json)
+            for messaging in flask.request.json['entry'][0]['messaging']:
+                msg = messaging['message']['text']
+                uid = messaging['sender']['id']
+                if RE_NUM.match(msg):
+                    choice = int(msg)
+                    r = amis.user_input(db, choice, uid)
+                else:
+                    r = amis.lookup(db, msg, uid)
+                send_fb_msg(uid, r)
+                return flask.Response(status=200)
+        except:
+            app.logger.info(flask.request.json)
+            return flask.Response(status=400)
+
+def send_fb_msg(uid, msg):
+    url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s' % FB_APP_TOKEN
+    data = {'recipient': {'id': uid},
+            'message': {'text': msg}}
+    app.logger.info(data)
+    r = requests.post(url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
+    if r.status_code != requests.codes.ok:
+        app.logger.warn(str(r.status_code))
+        app.logger.warn(str(r.headers))
+
+
 @app.route('/callback', methods=['POST',])
 def line_callback():
     app.logger.info(flask.request.json)
-    app.logger.info(flask.request.headers)
     try:
         req = flask.request.json["result"][0]
     except:
@@ -44,12 +80,14 @@ def line_callback():
     if req["eventType"] == "138311609100106403":
         send_text([req["from"]], u"Nga'ayho!  Mikamsia to\n謝謝你使用阿美語萌典 Line 機器人!\n")
     elif req["eventType"] == "138311609000106303":
-        to = [req["content"]["from"]]
+        uid = req["content"]["from"]
         txt = req["content"]["text"].strip()
-        if isCJK('txt'):
-            send_text(to, '漢字查阿美語，查查查...還沒做好 :(')
+        if RE_NUM.match(txt):
+            choice = int(txt)
+            r = amis.user_input(db, choice, uid)
         else:
-            send_text(to, '阿美語查漢語，查查查...還沒做好 :(')
+            r = amis.lookup(db, txt, uid)
+        send_text(uid, r)
     return flask.Response(status=200)
 
 def send_text(to, text):
@@ -61,7 +99,6 @@ def send_text(to, text):
     events(to, content)
 
 def events(to, content):
-    import json
     app.logger.info(content)
     data = {
         "to": to,
@@ -71,95 +108,16 @@ def events(to, content):
     }
     r = requests.post(LINE_ENDPOINT + "/v1/events", data=json.dumps(data), headers=LINE_HEADERS)
     app.logger.info(r.text)
-
-def isCJK(s):
-    return re.match(r'[\u00-\uff]+', s) is None
-
-
-def iterrows(cur, uid):
-    global USER_LASTWORD
-    i = 1
-    r = ''
-    USER_LASTWORD[uid] = [None,]
-    for row in cur:
-        USER_LASTWORD[uid].append(row[0])
-        r += '%d. %s\n' % (i, row[0])
-        i += 1
     return r
 
-
-def lookup(db, s, uid):
-    global USER_LASTWORD
-    cur = db.cursor()
-    if isCJK(s):    # 漢語查阿美語
-        cur.execute('SELECT title FROM amis WHERE example IS NULL AND cmn LIKE ? ORDER BY LENGTH(cmn)', ('%%' +s+ '%%', ))
-        rows = cur.fetchall()
-        if len(rows) == 0:
-            return u'找不到這個詞。'
-        else:
-            return u'「' +s+ u'」可能的阿美語詞有:\n' + iterrows(rows, uid) + u'請輸入號碼查詢單字。'
-    else:           # 阿美語查字典
-        cur.execute('SELECT cmn FROM amis WHERE title=? AND example IS NULL', (s, ))
-        rows = cur.fetchall()
-        if len(rows) > 0:    # 找到了
-            USER_LASTWORD[uid] = [s,]
-            return iterrows(rows, '') + u'要看例句請輸入 0'
-        cur.execute('SELECT amis FROM fuzzy WHERE fuzz LIKE ?', ('%%' + fuzzme(s) + '%%', ))
-        rows = cur.fetchall()
-        if len(rows) == 0:
-            return u'找不到這個詞。'
-        else:
-            return u'請問你要找的是哪個詞?\n' + iterrows(rows, uid) + u'請輸入數字選擇。'
-
-def user_input(db, num, uid):
-    print u'>>> 使用者 [%s] 輸入了 %d' % (uid, num)
-    if uid not in USER_LASTWORD:
-        return u'請重新查詢單字。'
-    choices = USER_LASTWORD[uid]
-    if num + 1 > len(choices):
-        return u'請重新選擇。'
-    if num == 0:
-        return get_example(db, choices[0])
-    word = choices[num]
-    return lookup(db, word, uid)
-
-
-def get_example(db, s):
-    cur = db.cursor()
-    cur.execute('SELECT example, cmn FROM amis WHERE title=? AND example IS NOT NULL', (s,))
-    rows = cur.fetchall()
-    if len(rows) == 0:
-        return u'詞條 %s 沒有例句。' % s
-    i = 1
-    r = '%s:\n' % s
-    for row in cur:
-        r += '%d. %s\n' % (i, row[0])
-        r += '    %s\n' % (row[1],  )
-        i += 1
-    return r
-
-
-def testme():
-    db = connect_db()
-    cur = db.cursor()
-    print 'False ==', isCJK("nga'aiho")
-    print 'True ==', isCJK(u'貓')
-    print lookup(db, 'pusi', 'testuser')
-    print user_input(db, 1, 'testuser')
-    print lookup(db, 'posi', 'testuser')
-    print user_input(db, 0, 'testuser')
-    print lookup(db, 'pusi\'', 'testuser')
-    print user_input(db, 2, 'testuser')
-    print lookup(db, 'pusi^', 'testuser')
-    print user_input(db, 3, 'testuser')
-    print lookup(db, u'貓', 'testuser')
-    print user_input(db, 2, 'testuser')
-    print user_input(db, 0, 'testuser')
-    sys.exit(10)
 
 if __name__ == "__main__":
-    # testme()
     config = ConfigParser.ConfigParser()
+    try:
+        (FB_TOKEN, FB_APP_TOKEN) = [x.strip() for x in open('fbtoken.cfg')]
+    except:
+        print u'請修改 fbtoken.cfg'
+        raise
     try:
         config.read('linebot.cfg')
         LINE_HEADERS = {
@@ -172,6 +130,6 @@ if __name__ == "__main__":
         raise
     app.config['JSON_AS_ASCII'] = False     # JSON in UTF-8
     app.config['DEBUG'] = True
-    context = ('cert1.pem', 'privkey1.pem') # Copy /etc/letsencrypt/live/ files to current dir
-    app.run(host = '0.0.0.0', threaded=False, port=8443, ssl_context=context)
+    context = ('fullchain.pem', 'privkey.pem') # Copy /etc/letsencrypt/live/ files to current dir
+    app.run(host = '0.0.0.0', threaded=False, port=443, ssl_context=context)
     print 'Shutdown...'
