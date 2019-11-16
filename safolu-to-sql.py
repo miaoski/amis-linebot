4e10cace920f8344886847bb1a6e817ed731ef59
Lin Zhemin <miaoski@gmail.com>
2019-11-16 20:28:56 +0800

    add stem into dict-safolu.sq3

--- safolu-to-sql.py
+++ safolu-to-sql.py
@@ -31,7 +31,7 @@ def load_amis():
                 .replace('"def":', '"f":') \
                 .replace('`', '') \
                 .replace('~', '')
-        cur.execute('INSERT INTO amis VALUES (?,?,?)', (title, ' '.join(cmn), content))
+        cur.execute('INSERT INTO amis VALUES (?,?,?,?)', (title, word.get('stem', title), ' '.join(cmn), content))
     conn.commit()
 
 
@@ -50,8 +50,9 @@ if __name__ == '__main__':
     cur = conn.cursor()
     cur.execute('DROP TABLE amis');
     cur.execute('DROP TABLE fuzzy');
-    cur.execute('CREATE TABLE amis (title text, cmn text, json text)')
+    cur.execute('CREATE TABLE amis (title text, stem text, cmn text, json text)')
     cur.execute('CREATE INDEX amis_title ON amis (title)')
+    cur.execute('CREATE INDEX amis_stem ON amis (stem)')
     cur.execute('CREATE TABLE fuzzy (fuzz text, amis text)')
     cur.execute('CREATE INDEX fuzzy_fuzz ON fuzzy (fuzz)')
     conn.commit()
